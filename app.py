cacc8a8f9cb514df5da8097824e9aaaaf5f5d12e
Michelle Mesquita <mac@MacBook-Pro-de-Mac.local>
2023-01-30 01:59:26 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,68 @@
+import os
+import openai
+from dotenv import load_dotenv
+from flask import Flask, request
+from twilio.rest import Client 
+
+
+app = Flask(__name__)
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+account_sid = os.getenv("account_sid")
+auth_token = os.getenv("auth_token")
+from_str=os.getenv("from")
+to_str=os.getenv("to")
+
+
+client = Client(account_sid, auth_token) 
+ 
+
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    
+    try:
+        incoming_msg = request.form.get("Body").lower()
+
+        resp = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=incoming_msg,
+        temperature=0.3,
+        max_tokens=150,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+        )
+        
+        quote = str(resp["choices"][0]["text"])
+
+        message = client.messages.create( 
+                                from_=from_str,  
+                                body=quote,      
+                                to=to_str
+                            ) 
+    
+        
+        # print(message.sid)
+
+
+        return str(resp["choices"][0]["text"])
+
+    except:
+        quote = str("Erro 🤖")
+
+        message = client.messages.create( 
+                                from_=from_str,  
+                                body=quote,      
+                                to=to_str
+                            ) 
+
+    
+
+
+
+
+if __name__ == '__main__':
+   app.run(debug = True, port=8070)
+
